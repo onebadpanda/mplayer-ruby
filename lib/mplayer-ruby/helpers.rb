290a2f1a33be97a0e7c69d05a0271f18c872dc27
One Bad Panda <accounts@onebadpanda.com>
2014-01-14 14:54:43 -0500

    which method to determine path to mplayer

---
+++ lib/mplayer-ruby/helpers.rb
@@ -0,0 +1,18 @@
+module MPlayer
+  module Helpers
+
+    # Returns the path to a given command
+    # This works on multiple platforms.
+    # Taken from: http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        }
+      end
+      return nil
+    end
+  end
+end
